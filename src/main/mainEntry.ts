1180fc3ecca10b88dba4771e4e5aae6966a61116
cell <cellinlab@gmail.com>
2023-05-10 10:57:53 +0800

    feat: load node modules in renderer process

--- src/main/mainEntry.ts
+++ src/main/mainEntry.ts
@@ -1,8 +1,23 @@
 import { app, BrowserWindow } from 'electron';
 
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
+
 let mainWindow: BrowserWindow;
 
 app.whenReady().then(() => {
-  mainWindow = new BrowserWindow({});
+  const config = {
+    webPreferences: {
+      nodeIntegration: true, // Load node modules in renderer process
+      wbeSecurity: false,
+      allowRunningInsecureContent: true,
+      contextIsolation: false,
+      webviewTag: true,
+      spellcheck: false,
+      disableHtmlFullscreenWindowResize: true,
+    },
+  };
+
+  mainWindow = new BrowserWindow(config);
+  mainWindow.webContents.openDevTools({ mode: 'undocked' });
   mainWindow.loadURL(process.argv[2]);
 });
